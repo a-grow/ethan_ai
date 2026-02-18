7fbbbda2bc5c1a87b3f5f64b1c86b32515f66c2b
a-grow <growandygrow@gmail.com>
2026-02-18 15:59:47 +0800

    Created Featured Sites subfolders with clean domain paths and updated links (no style changes)

---
+++ aurora-audio/_next/static/z93gZVErTgqSPBJWiZh0k/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/aurora-audio//_next/:path+",
+        "destination": "/aurora-audio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
