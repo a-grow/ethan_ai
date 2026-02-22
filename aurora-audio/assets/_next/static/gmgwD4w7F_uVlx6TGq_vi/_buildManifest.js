61225812b386b680017415a3be3c984fafce9142
a-grow <growandygrow@gmail.com>
2026-02-22 23:53:27 +0800

    Fix AuroraAudio React chunk pathing bug

--- aurora-audio/assets/_next/static/gmgwD4w7F_uVlx6TGq_vi/_buildManifest.js
+++ aurora-audio/assets/_next/static/gmgwD4w7F_uVlx6TGq_vi/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/aurora-audio/assets//_next/:path+",
+        "destination": "/aurora-audio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
