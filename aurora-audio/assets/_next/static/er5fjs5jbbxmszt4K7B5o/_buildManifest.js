c5df00bfb63aef2703bb6131df38c2459c5bc9b2
a-grow <growandygrow@gmail.com>
2026-02-22 23:38:22 +0800

    Mirror AuroraAudio build output into portfolio path

--- aurora-audio/assets/_next/static/er5fjs5jbbxmszt4K7B5o/_buildManifest.js
+++ aurora-audio/assets/_next/static/er5fjs5jbbxmszt4K7B5o/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/aurora-audio//_next/:path+",
-        "destination": "/aurora-audio/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
